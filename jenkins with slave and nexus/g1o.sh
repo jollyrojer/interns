a2a29b05b05cc80922797f3a7314f7578d72bc3d
ibrichko <ibrichko@integralads.com>
2019-08-28 16:47:59 +0300

    update

--- jenkins with slave and nexus/g1o.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/sh
-curl -L https://download.sonatype.com/nexus/nexus-professional-bundle-latest.tar.gz > nxs/nexus.tgz
-curl -L http://mirrors.jenkins.io/war-stable/latest/jenkins.war > jnk/jenkins.war
-docker-compose up
